9776e3ac63c586feda75f50e22869128c083da62
Akmalzhon Nurmakhamadov <76267204+Akmaljon03@users.noreply.github.com>
2024-04-30 18:50:07 +0600

    SKED

---
+++ src/main/java/sked/ecommerce/Streamline.java
@@ -0,0 +1,16 @@
+package sked.ecommerce;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Streamline{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Streamline.class, args);
+    }
+
+    }
+
+
+
+    /*/
