34eb87dee15ba50537ce1f399a0bc0ba59820dc7
Takhirzhon <tashmatovtahir@gmail.com>
2024-05-16 02:44:13 +0600

    done

--- src/main/java/sked/ecommerce/Streamline.java
+++ src/main/java/sked/ecommerce/Streamline.java
@@ -1,10 +1,8 @@
 package sked.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "sked.ecommerce.repository")
 public class Streamline{
 
     public static void main(String[] args) {
